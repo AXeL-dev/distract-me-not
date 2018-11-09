da1ab739a84fa27d37972b7e2ab7a76b00e2570b
AXeL-dev <anass_denna@hotmail.fr>
2018-11-09 23:41:14 +0100

    better icons

--- src/js/common.js
+++ src/js/common.js
@@ -5,17 +5,20 @@ var hasClass = function(el, cls) {
     }
     return el.className.match(new RegExp("(\\s|^)" + cls + "(\\s|$)")) ? true : false
 };
+
 var addClass = function(el, cls) {
     if (!hasClass(el, cls)) {
         el.className += " " + cls
     }
 };
+
 var removeClass = function(el, cls) {
     if (hasClass(el, cls)) {
         var reg = new RegExp("(\\s|^)" + cls + "(\\s|$)");
         el.className = el.className.replace(reg, " ")
     }
 };
+
 var replaceClass = function(el, cls, newcls) {
     if (hasClass(el, cls)) {
         el.className = el.className.replace(" " + cls, newcls)
