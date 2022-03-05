f11cddef682a5f054ce70970ca8bb18f1e933df4
AXeL-dev <contact.axel.dev@gmail.com>
2022-03-05 18:55:49 +0100

    chore: restructure unit tests

--- src/__tests__/components/App.test.js
+++ src/__tests__/components/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from '../App';
+import App from 'App';
 
 it('renders nothing when accessAllowed prop is not defined', () => {
   const { container } = render(<App />);
