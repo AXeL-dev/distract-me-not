AXeL-dev/distract-me-not

commit a870590258e7e97fe74e3be9a29201cb1011e20b

chore: minor fixes for Edge browser

src/helpers/block.js
@@ -55,7 +55,7 @@ export const defaultUnblock = {
 };
 
 export function isAccessible(url) {
-  return url && !url.startsWith('about:') && !/^(?:file|chrome|moz-extension|chrome-extension):\/\//i.test(url);
+  return url && !/^(?:file|about|chrome|moz-extension|chrome-extension|edge):\/\//i.test(url);
 }
 
 export function isPageReloaded() {

src/helpers/url.js
@@ -30,7 +30,7 @@ export function checkFaviconLink(faviconLink) {
 }
 
 export function hasValidProtocol(url) {
-  return /^(?:ftps?|https?|file):\/\//i.test(url) || url.startsWith("about:");
+  return /^(?:ftps?|https?|file|about|chrome|edge):\/\//i.test(url);
 }
 
 export function getValidUrl(url) {

commit 94a3c759a1b2f91d9cc6afd56e751fe7f16ef0e8

chore: fix regression introduced in a870590

src/helpers/block.js
@@ -55,7 +55,7 @@ export const defaultUnblock = {
 };
 
 export function isAccessible(url) {
-  return url && !/^(?:file|about|chrome|moz-extension|chrome-extension|extension|edge):\/\//i.test(url);
+  return url && !/^((file|chrome|moz-extension|chrome-extension|extension|edge):\/\/|about:)/i.test(url);
 }
 
 export function isPageReloaded() {

src/helpers/url.js
@@ -30,7 +30,7 @@ export function checkFaviconLink(faviconLink) {
 }
 
 export function hasValidProtocol(url) {
-  return /^(?:ftps?|https?|file|about|chrome|edge|extension):\/\//i.test(url);
+  return /^((ftps?|https?|file|chrome|edge|extension):\/\/|about:)/i.test(url);
 }
 
 export function getValidUrl(url) {